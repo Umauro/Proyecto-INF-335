3f0eb1bf25e0a961dec9593eec4d9c1ca002a253
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2020-03-09 16:52:27 -0300

    intento de scrapper

--- scrapper/script.py
+++ scrapper/script.py
@@ -1,5 +1,6 @@
 from devir import DevirScrapper, DevirTransformer
 from skyship import SkyshipScrapper, SkyshipTransformer
+from warpig import WarpigScrapper, WarpigTransformer
 from dataloader import DataLoader, GAMES_UPSERT, PRICES_INSERT
 
 devir_scrapper = DevirScrapper()
@@ -17,3 +18,11 @@ df_skyship = skyship_transformer.transform_data()
 skyship_dataloader = DataLoader(df_skyship)
 skyship_dataloader.upsert_games(GAMES_UPSERT)
 skyship_dataloader.insert_price(PRICES_INSERT)
+
+warpig_scrapper = WarpigScrapper()
+df_warpig = warpig_scrapper.get_contents()
+warpig_transformer = WarpigTransformer(df_warpig)
+df_warpig = warpig_transformer.transform_data()
+warpig_dataloader = DataLoader(df_warpig)
+warpig_dataloader.upsert_games(GAMES_UPSERT)
+warpig_dataloader.insert_price(PRICES_INSERT)
